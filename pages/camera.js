945e892e7f4e7127db524b70e588e6b33c9885a6
rayhan <prog.rayhan1@gmail.com>
2022-04-16 11:31:01 +0600

    first commit

---
+++ pages/camera.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef } from "react";
+
+function camera() {
+  let videoRef = useRef(null);
+  let photoRef = useRef(null);
+
+  // get access to user Webcam
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const takePicture = () => {
+    const width = 400;
+    const height = width / (16 / 9);
+
+    let video = videoRef.current;
+
+    let photo = photoRef.current;
+
+    photo.width = width;
+
+    photo.height = height;
+
+    let ctx = photo.getContext("2d");
+
+    ctx.drawImage(video, 0, 0, width, height);
+  };
+
+  const clearImage = () => {
+    let photo = photoRef.current;
+
+    let ctx = photo.getContext("2d");
+
+    ctx.clearRect(0, 0, photo.width, photo.height);
+  };
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
+  return (
+    <div>
+      <h1>Camera Selfie App in React</h1>
+ 
+      <video ref={videoRef}></video>
+ 
+      <button onClick={takePicture}>Take Picture</button>
+ 
+      <canvas ref={photoRef}></canvas>
+ 
+      <button onClick={clearImage}>Clear Image</button>
+ 
+      <br/><br/>
+    </div>
+  );
+}
+
+export default camera;
---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+/**
+ * WebCam from https://www.npmjs.com/package/react-webcam
+ */
+import React from "react";
+import Camera from "./camera"
+import Webcam from "react-webcam";
+import styles from '../styles/Home.module.css'
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import PhotoCamera from '@mui/icons-material/PhotoCamera';
+import Stack from '@mui/material/Stack';
+
+const Input = styled('input')({
+  display: 'none',
+});
+
+export default function Home() {
+
+  return (
+    <>
+      {/* <Webcam />
+      <Stack direction="row" alignItems="center" spacing={2}>
+        <label htmlFor="contained-button-file">
+          <Input accept="image/*" id="contained-button-file" multiple type="file" />
+          <Button variant="contained" component="span">
+            Upload
+          </Button>
+        </label>
+        <label htmlFor="icon-button-file">
+          <Input accept="image/*" id="icon-button-file" type="file" />
+          <IconButton color="primary" aria-label="upload picture" component="span">
+            <PhotoCamera />
+          </IconButton>
+        </label>
+      </Stack> */}
+      <Camera/>
+    </>
+  )
+}
